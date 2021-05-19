99e6e4d57bfa53a9b76945ec1437cc58ffb520d8
Sarunas <sar.dovydaitis@hotmail.com>
2021-05-19 19:51:11 +0300

    socials

---
+++ js/data/socialsData.js
@@ -0,0 +1,32 @@
+const socialsData = [
+    {
+        href: '#',
+        icon: 'facebook'
+    },
+    {
+        href: '#',
+        icon: 'twitter'
+    },
+    {
+        href: '#',
+        icon: 'dribbble'
+    },
+    {
+        href: '#',
+        icon: 'tumblr'
+    },
+    {
+        href: 'https://www.linkedin.com',
+        icon: 'linkedin'
+    },
+    {
+        href: '#',
+        icon: 'instagram'
+    },
+    {
+        href: '#',
+        icon: 'rss'
+    },
+];
+
+export { socialsData }
